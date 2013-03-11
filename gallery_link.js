bcb4ef5076bf8ca34dd8fac74f3d441c05230ba8
yannickoo <drupal@yannickoo.de>
2013-03-11 04:03:20 +0100

    Initial commit of the 2.x version.

--- gallery_link.js
+++ gallery_link.js
@@ -1,17 +1,16 @@
 (function ($) {
 
-  Drupal.behaviors.gallery_link = {
+  Drupal.behaviors.galleryLink = {
     attach: function (context, settings) {
-      // We bind a click event to all items with a "data-gallery-id" attribute
-      $('a[data-gallery-id]').bind('click', function(e) {
-        currentGallery = Drupal.settings.gallery_link[$(this).attr('data-gallery-id')];
-        // We access the gallery with the gallery id and put it into the fancybox
+      // We bind a click event to all items with a "data-gallery-id" attribute.
+      $('a[data-gallery-link]').bind('click', function(e) {
+        var currentGallery = settings.galleryLink[$(this).attr('data-gallery-link')];
+        // Trigger fancyBox, pass images and optins.
         $.fancybox(currentGallery['images'], currentGallery['options']);
-        // We prevend the default behavior of the link
+        // We prevend the default behavior of the link.
         e.preventDefault();
       });
     }
-  }
+  };
 
 })(jQuery);
- 
